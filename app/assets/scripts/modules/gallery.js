4a8f55aa07f199213b187f17b6fd63d38bb4ff52
paul <paulrooney60@gmail.com>
2020-08-31 16:20:25 +0100

    fixed z-index error making links unclickable

--- app/assets/scripts/modules/gallery.js
+++ app/assets/scripts/modules/gallery.js
@@ -1,8 +1,9 @@
 class Gallery{
 	constructor(){
-       this.eventArea = document.querySelector(".cert__gallery")
-       this.mainImg = document.querySelector(".cert__gallery-main-pic")
-       this.thumbs = document.querySelector(".cert__thumbs-container")
+       this.eventArea = document.querySelector(".cert__gallery")//galley event section
+       this.mainImg = document.querySelector(".cert__gallery-main-pic")//image to change
+       this.thumbs = document.querySelector(".cert__thumbs-container")//area to load thumbs
+       //array of thumbs
        this.imgArray = [['assets/images/certs/intro-to-html5.jpg','passed intro to html5'],
                         ['assets/images/certs/intro-to-css3.jpg','passed intro to css3'],
                         [ 'assets/images/certs/Interactivity-with-JavaScript.jpg','passed interactivity with javaScript'],
@@ -20,17 +21,17 @@ class Gallery{
 	
 	events() {
 		
-		this.eventArea.addEventListener("click", e => this.toggleImg(e))
-		this.thumbs.addEventListener("click", e => this.thumbChange(e))
+		this.eventArea.addEventListener("click", e => this.toggleImg(e))//event for left/right nav
+		this.thumbs.addEventListener("click", e => this.thumbChange(e))//event for selection by thumbs
 
 		
 	}
- 
+ //load images to thumb container
  loadImgs(){
   let count = 0
-  for(const img of this.imgArray){
+  for(const img of this.imgArray){//add default thumb
   let thumb = `<img src="${img[0]}" alt="${img[1]}" id="${count}" name="cert ${count}" class="cert__thumb">`
-  if(count === this.currentImage){
+  if(count === this.currentImage){//add styled active thumb
    thumb = `<img src="${img[0]}" alt="${img[1]}" id="${count}" name="cert ${count}" class="cert__thumb cert__thumb--active">`
    
    
@@ -42,7 +43,8 @@ class Gallery{
 	}
  
 	toggleImg(e) {
-   this.activeThumb = document.querySelector('.cert__thumb--active')//call fresh active thumb
+   this.activeThumb = document.querySelector('.cert__thumb--active')//call current active thumb
+   //handle slideshow toggle left/right
 	if(e.target.classList.value.includes('left') || e.target.parentNode.classList.value.includes('left')){
   if (this.currentImage > 0){
     this.currentImage--
@@ -65,12 +67,13 @@ class Gallery{
 		
 	}
 	}
+ //handle change by thumb
  thumbChange(e){
-  this.activeThumb = document.querySelector('.cert__thumb--active')//call fresh active thumb
-  if(e.target.name.includes('cert')){
+  this.activeThumb = document.querySelector('.cert__thumb--active')//call current active thumb
+  if(e.target.name.includes('cert')){//avoid misclicks on other elements
   this.mainImg.src = e.target.src
   this.mainImg.alt = e.target.alt
-  this.currentImage = parseInt(e.target.id)
+  this.currentImage = parseInt(e.target.id)//update current image
   this.activeThumb.classList.remove('cert__thumb--active')//remove current active thumb
   e.target.classList.add('cert__thumb--active')//add active class to new active tab
   }
